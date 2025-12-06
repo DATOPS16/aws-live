aa40f5b0e2870e7aa79fe9b9c6f5dbf7780a79a3
DATOPS16 <wongps-wp22@student.tarc.edu.my>
2025-12-06 11:13:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cixe3iilbndj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "pengseung-employee"
+customregion = "us-east-1"
+
